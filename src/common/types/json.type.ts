283fcae6231fc367cf94bc511a6f052dccb33432
Mykola Rudnytskyi <rudcol1997@gmail.com>
2022-12-21 13:57:12 +0100

    JSON type

---
+++ src/common/types/json.type.ts
@@ -0,0 +1,5 @@
+export type JSONPrimitive = string | number | boolean | null
+export type JSONValue = JSONPrimitive | JSONObject | JSONArray
+export type JSONObject = { [member: string]: JSONValue }
+export type JSONArray = JSONValue[]
+export type JSONType = JSONObject | JSONArray
