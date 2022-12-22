663a915566e091b330aea1888aae921e239c6149
Mykola Rudnytskyi <rudcol1997@gmail.com>
2022-12-22 20:24:15 +0100

    Log collection basic implementation

--- src/collector/collector.resolver.ts
+++ src/collector/collector.resolver.ts
@@ -1,13 +1,27 @@
-import { Args, Query, Resolver } from '@nestjs/graphql'
-import { LogSeverity } from '../common/enums'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { GraphQLJSONObject } from 'graphql-type-json'
+import type { JSONObject } from '../common/types'
 import { CollectorService } from './collector.service'
+import { CollctOptionsInput } from './dto/collect-options.input'
 
 @Resolver()
 export class CollectorResolver {
   constructor(private readonly collectorService: CollectorService) {}
 
-  @Query(() => String)
-  helloWorld(@Args('level', { type: () => LogSeverity }) logSeverity: LogSeverity) {
-    return 'Hello, world! ' + logSeverity
+  // Added temporarily to fix "GraphQLError: Query root type must be provided."
+  // TODO please, remove this when some "feature" query is added
+  @Query(() => Boolean, {
+    deprecationReason: 'to be removed',
+  })
+  root() {
+    return true
+  }
+
+  @Mutation(() => Boolean)
+  collectLog(
+    @Args('data', { type: () => GraphQLJSONObject }) data: JSONObject,
+    @Args('options', { type: () => CollctOptionsInput }) { logSeverity }: CollctOptionsInput,
+  ): boolean | Promise<boolean> {
+    return this.collectorService.push(data, logSeverity, new Date())
   }
 }
