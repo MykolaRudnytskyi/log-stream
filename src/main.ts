1ab5c2c905756cc203718d299c9ae3bdddf60337
Mykola Rudnytskyi <rudcol1997@gmail.com>
2022-12-22 17:46:15 +0100

    Remove some testing code

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { createLogFiles } from './helpers/create-log-files'
-import { Queue } from './queue'
 
 async function bootstrap() {
-  const q = new Queue()
-  const a = await q.pushAndWait(
-    Queue.wrapToItem(async () => {
-      return 1
-    }),
-  )
-
-  console.log(a)
-
   await createLogFiles()
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
