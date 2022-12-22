7caa6634884de8894335ba6f0984516a6f250af5
Mykola Rudnytskyi <27192591+MykolaRudnytskyi@users.noreply.github.com>
2022-12-22 18:45:58 +0100

    `Queue` feature (#3)
    * Basic queue (event-based) implementation
    
    * Move `Item` class to separate file
    
    * Improve generics in `Queue` class
    
    * Remove some testing code
    
    * Use `EventEmiter` as field

---
+++ src/queue/queue.ts
@@ -0,0 +1,55 @@
+import { EventEmitter } from 'events'
+import { Item } from './item'
+
+enum Events {
+  PROCESS = 'process',
+}
+
+export class Queue {
+  private ee = new EventEmitter()
+  private queue: Item[] = []
+
+  constructor() {
+    this.spawnNewItemListener()
+  }
+
+  private async processRecurrently(): Promise<void> {
+    const item = this.queue.shift()
+
+    if (item) {
+      try {
+        const res: unknown = await item.action()
+        item.executor?.resolve?.(res)
+      } catch (e) {
+        item.executor?.reject?.(e)
+      }
+
+      return this.processRecurrently()
+    }
+  }
+
+  private spawnNewItemListener() {
+    this.ee.once(Events.PROCESS, async () => {
+      await this.processRecurrently()
+      this.spawnNewItemListener()
+    })
+  }
+
+  static wrapToItem<T>(action: Item<T>['action']) {
+    return new Item<T>(action)
+  }
+
+  push(item: Item) {
+    this.queue.push(item)
+    this.ee.emit(Events.PROCESS)
+  }
+
+  pushAndWait<T>(item: Item<T>) {
+    type Result = Awaited<ReturnType<typeof item['action']>>
+    return new Promise<Result>((resolve, reject) => {
+      item.executor = { resolve, reject }
+      this.queue.push(item)
+      this.ee.emit(Events.PROCESS)
+    })
+  }
+}
